cbc84b06bc5713f2a1fc9728899c4eb3acfb79af
Sinipelto <toni.blafield@tuni.fi>
2021-02-05 11:22:29 +0200

    added ex4 skeleton

---
+++ Exercise4/main.cpp
@@ -0,0 +1,40 @@
+﻿// Crypto++ (CryptoPP) Library (cryptlib)
+// Compiled as static debug and release builds
+// linked statically to this project
+// This project built with static linking
+// Linking debug/release build, depending on this project setting
+#ifdef _DEBUG
+#  pragma comment ( lib, "cryptlibd" )
+#else
+#  pragma comment ( lib, "cryptlib" )
+#endif
+
+// STD C++ headers
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+#include <sstream>
+#include <vector>
+
+// CryptoPP headers
+#include <cryptlib.h>
+#include <secblock.h>
+#include <osrng.h>
+#include <modes.h>
+#include <sha3.h>
+#include <hex.h>
+#include <pwdbased.h>
+#include <filters.h>
+#include <threefish.h>
+#include <aes.h>
+#include <files.h>
+#include <gcm.h>
+#include <ccm.h>
+#include <simple.h>
+
+int main()
+{
+}
