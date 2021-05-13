e8fe4300e1dcd00245b64c2963d8856b53c76dfd
Sinipelto <toni.blafield@tuni.fi>
2021-05-13 13:56:24 +0300

    unit tests for pwned api breach service.

---
+++ SecureWebApp.Tests/Template.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace SecureWebApp.Tests
+{
+    public class Template
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
